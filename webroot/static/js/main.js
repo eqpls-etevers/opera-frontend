6f8264e56151452fa8a03a6033ec11eaa4892e9c
jzide <jzide@JzWork>
2024-08-27 15:44:45 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -3,25 +3,30 @@
 var main = () => {
 	document.getElementById("eqpls-access-token").innerText = window.common.auth.accessToken;
 
+	window.location.replace("/aria/auth/login");
+
+
+/*
 	window.common.wsock.connect(
 		"/router/websocket",
 		(socket, data) => {
 			console.log(data);
 		}, (socket) => {
 			socket.sendData("hello", "world");
+			window.location.replace("/aria/auth/login");
 		}
 	);
+*/
 
-
-	fetch(`/aria/auth/authorize`, {
-		headers: window.common.auth.apiHeader
-	}).then((res) => {
+/*
+	fetch(`/aria/auth/login`).then((res) => {
 		if (res.ok) { return res.json(); }
 		console.error(res);
 		throw res
 	}).then((data) => {
 		console.log(data);
 	});
+*/
 };
 
 window.common.init(() => {
