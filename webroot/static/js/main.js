c242187f7558926be9361307377af9343273edb0
jzide <jzide@JzBook>
2024-08-28 20:22:54 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -14,16 +14,15 @@ function main() {
 		aaDom.appendChild(dom);
 	});
 
-	window.opera.getRegions((regions) => {
-		regions.forEach((region) => {
-			region.print();
-			region.getProject((projects) => {
-				projects.forEach((project) => {
-					project.print();
-					project.getCatalogs((catalogs) => {
-						catalogs.forEach((catalog) => {
-							catalog.print();
-						});
+	let regions = window.opera.getRegions();
+	regions.forEach((region) => {
+		region.print();
+		region.getProject((projects) => {
+			projects.forEach((project) => {
+				project.print();
+				project.getCatalogs((catalogs) => {
+					catalogs.forEach((catalog) => {
+						catalog.print();
 					});
 				});
 			});
