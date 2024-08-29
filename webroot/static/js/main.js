eb15db6fbbb9a0962d8ca2e08ef6c7bd69d0cb6d
jzide <jzide@JzBook>
2024-08-29 20:35:08 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -22,6 +22,7 @@ function main() {
 				project.print();
 				project.getCatalogs((catalogs) => {
 					catalogs.sortDescBy('name');
+					catalogs.sortAscBy('name');
 					catalogs.forEach((catalog) => {
 						catalog.print();
 					});
