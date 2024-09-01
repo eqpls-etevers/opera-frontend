d5aa465e1f73ab84de1973dab361b5f484f79e60
jzide <jzide@JzWork>
2024-09-01 17:46:51 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -21,7 +21,7 @@ async function main() {
 
 	let projects = await region.getProjects();
 	projects.print();
-	for (let i=0; i<projects.lenth; i++) {
+	for (let i=0; i<projects.length; i++) {
 		let project = projects[i];
 		project.print();
 	}
