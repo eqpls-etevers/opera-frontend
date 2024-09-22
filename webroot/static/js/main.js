8d6a2e6d12b64db2a800ec5ba3aaf3d5e9940321
jzide <jzide@JzWork>
2024-09-22 21:20:33 +0900

    update

--- webroot/static/js/main.js
+++ webroot/static/js/main.js
@@ -16,11 +16,10 @@ Opera.init(async () => { // main task
 		regionsDom.appendChild(dom); // append a created "p" tag dom to region information dom
 	}
 
-	//let buckets = await window.module.data.getBuckets();
-	//buckets.print();
+	let buckets = await Module.Data.getBuckets();
+	buckets.print();
 
 	regions[0].checkpoint(); // set first region to checkpoint "window.opera.Region"
-
 	let projects = await Opera.Region.getProjects(); // get project list by "await" async code format
 	projects.print(); // print project list to console
 	projects.forEach(async (project) => { // iterate project list by ".forEach(async (item) => { ... })" code format
